e4ed11dfd2aec93f34c347b145f092528cf6addf
Dennis <zhang_dennis1999@hotmail.com>
2021-03-08 19:05:42 -0800

    comments added

--- backend-parser/app.py
+++ backend-parser/app.py
@@ -11,12 +11,21 @@ def parse_skills():
     req = request.get_json()
     user_input = req.get('data')
 
+    """
+    third party api url
+    """
     url = "https://api.iki.ai/api/skills_extraction/"
 
+    """
+    set up payload to post to api
+    """
     payload = {
         "text": user_input
     }
 
+    """
+    create request headers
+    """
     headers = {
         'Content-Type': 'application/json'
     }
